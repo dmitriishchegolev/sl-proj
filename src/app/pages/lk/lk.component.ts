1004deeda978d6956b9aab73800b84c9ceaa59af
user2 <dmitriishchegolev@yandex.ru>
2023-09-13 18:11:41 +0300

    init

---
+++ src/app/pages/lk/lk.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lk',
+  templateUrl: './lk.component.html',
+  styleUrls: ['./lk.component.scss']
+})
+export class LkComponent {
+
+}
